d9b191d9275aaea47b2817c986bde9379da74b8f
Masih Nasiri <masih_nasiri4@yahoo.com>
2024-09-24 12:33:27 +0330

    Add files via upload

---
+++ 6. Detection of the presence or absence of single-phase and two-phase faults and fault location identification_KNN method.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn import  preprocessing
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from  sklearn import  metrics
+df = pd.read_csv('finalExcel6.csv')
+df=shuffle(df,random_state=0)
+X = df[['t', 'ia', 'ib', 'ic','isq','isd', 'vsd', 'vsq', 'ha', 'hb', 'hc', 'speed', 'angle', 'torque']].values
+Y = df['output'].values
+X = preprocessing.StandardScaler().fit(X).transform(X.astype(float))
+X_train , X_test , Y_train , Y_test = train_test_split(X , Y , test_size=0.2 ,random_state=4)
+print('Train Set :',X_train.shape,Y_train.shape)
+print('Test set :',X_test.shape,Y_test.shape)
+k=9
+neigh = KNeighborsClassifier(n_neighbors=k).fit(X_train,Y_train)
+yhat = neigh.predict(X_test)
+print('Train Set acc: ' , metrics.accuracy_score(Y_train,neigh.predict(X_train)) )
+print('Test Set acc: ' , metrics.accuracy_score(Y_test,yhat))
---
+++ 6. Detection of the presence or absence of single-phase and two-phase faults and fault location identification_SVM method.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn import metrics
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+zf = pd.read_csv('finalExcel6.csv')
+zf = shuffle(zf, random_state=0)
+X = zf[['t', 'ia', 'ib', 'ic','isq','isd', 'vsd', 'vsq', 'ha', 'hb', 'hc', 'speed', 'angle', 'torque']].values
+Y = zf['output'].values
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=4)
+print('Train Set :', X_train.shape, Y_train.shape)
+print('Test set :', X_test.shape, Y_test.shape)
+clf = svm.SVC(kernel='poly')
+#clf = svm.SVC(kernel='sigmoid')
+#clf = svm.SVC(kernel='rbf')
+clf.fit(X_train, Y_train)
+yhat = clf.predict(X_test)
+print('Train Set acc: ', metrics.accuracy_score(Y_train, clf.predict(X_train)))
+print('Test Set acc: ', metrics.accuracy_score(Y_test, yhat))
